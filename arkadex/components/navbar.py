b43913ed1df2da93f547c061a926e69784f6b049
ches24 <andresg2409@gmail.com>
2025-05-25 20:06:34 +0200

    grid_layout - hero actualizado

--- arkadex/components/navbar.py
+++ arkadex/components/navbar.py
@@ -1,19 +1,39 @@
 import reflex as rx
 
+from arkadex.styles import styles
+
+
 def navbar()->rx.Component:
     return rx.vstack(
+        rx.spacer(),
         rx.text(
-            "Where legends rise"
+            "Where legends rise",
+            style=styles.font_title_style,
         ),
         rx.hstack(
+            rx.spacer(),
             rx.hstack(
+                rx.image(
+                    src="user-plus.png",
+                    height="1.5em",
+                ),
                 "Registrate",
+                style=styles.font_horizontal_button_style,
             ),
             rx.hstack(
+                rx.image(
+                    src="chart-simple.png",
+                    height="1.5em",
+                ),
                 "Informate",
+                style=styles.font_horizontal_button_style,
             ),
+            spacing="8",
+            # bg="orange",
+            align="end",
+            width="70%",
         ),
-        bg="blue",
+        # bg="blue",
         width="100%",
         height="100%",
     )
--- arkadex/views/hero.py
+++ arkadex/views/hero.py
@@ -1,59 +1,118 @@
 import reflex as rx
 
+from arkadex.components.button import button_round
+from arkadex.components.navbar import navbar
+from arkadex.styles import styles
 from arkadex.styles.styles import MAX_HEIGHT
 
 
 def hero()->rx.Component:
     return rx.hstack(
         rx.container(
-            bg="red",
+            rx.image(
+                src="logoVertical.png",
+                width="20vh",
+                margin_top="5vh",
+            ),
+            # bg="red",
             width="15vw",
             height="100%",
         ),
         rx.grid(
             rx.box(
+                navbar(),
                 grid_column="1/14",
                 grid_row="1/4",
-                bg="orange",
+                # bg="orange",
                 z_index="100",
             ),
             rx.flex(
+                rx.text(
+                    "Donde los valientes no juegan... compiten y destrozan",
+                    style=styles.font_vertical_style,
+                ),
+                align="end",
+                justify="end",
                 grid_column="1/2",
                 grid_row="4/-1",
-                bg="purple",
+                # bg="purple",
                 z_index="100",
             ),
             rx.container(
+                rx.grid(
+                    # gcolumns="8",
+                    # rows="8",
+                    bg="grey",
+                    width="100%",
+                    height="100%",
+
+                ),
+                background_image="url(bg_container.png)",
+                background_repeat="no-repeat",
+                background_size="80% 100%",
+                background_position="right",
                 grid_column="2/-1",
                 grid_row="2/14",
-                bg="cadetblue",
+                # bg="cadetblue",
                 z_index="99",
             ),
             rx.hstack(
-                grid_column="12/-1",
-                grid_row="6/14",
-                bg="darkgreen",
+                rx.box(
+                    rx.vstack(
+                        button_round(
+                            "gamepad-modern",
+                        ),
+                        rx.text(
+                            "Ver equipos",
+                            style=styles.font_vertical_button_style,
+                        ),
+
+                        align="center"
+                    ),
+                    # bg="darkgreen",
+                    z_index="100",
+                ),
+                rx.spacer(),
+                rx.box(
+                    rx.vstack(
+                        button_round(
+                            "alien-8bit"
+                        ),
+                        rx.text(
+                            "Ver partidas",
+                            style=styles.font_vertical_button_style,
+                        ),
+                        align="center"
+                    ),
+                    # bg="green",
+                    z_index="99",
+                ),
+                grid_column="14/16",
+                grid_row="8/14",
+                # bg="darkgreen",
                 z_index="100",
+
+
             ),
             rx.flex(
                 grid_column="2/-1",
                 grid_row="14/-1",
-                bg="orchid",
+                # bg="orchid",
                 z_index="100",
             ),
             columns="16",
             rows="16",
-            bg="grey",
+            # bg="grey",
             width="80vw",
             height="100%",
         ),
         rx.container(
-            bg="green",
+            # bg="green",
             width="5vw",
             height="100%",
         ),
 
-        bg="violet",
+        # bg="violet",
         spacing="1",
         width="100%",
         height=MAX_HEIGHT
