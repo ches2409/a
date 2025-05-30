d9ed4f695b3c5cafc771d08d9edac420c39689d8
ches24 <andresg2409@gmail.com>
2025-05-30 02:50:39 +0200

    actualizacion, card_team con botones

--- arkadex/components/card_team.py
+++ arkadex/components/card_team.py
@@ -15,109 +15,123 @@ def card_team(
         gaming_style_2:str,
         gaming_style_3:str,
 )->rx.Component:
-    return rx.hstack(
-    rx.flex(
-        rx.image(
-            src=f"{image}.png",
-            height="100%",
+    return rx.vstack(
+        rx.hstack(
+            rx.flex(
+                rx.image(
+                    src=f"{image}.png",
+                    # max_height="none",
+                    height="100%",
+                    width="100%",
+                    object_fit="contain",
+                    object_position="right top",
+
+                ),
+                # align_content="flex-start",
+                # justify_content="flex-start",
+                position="realative",
+                height="35em",
+                width="35%",
+                # bg="red"
+            ),
+            rx.vstack(
+                rx.flex(
+                    rx.text(
+                        subtitle,
+                        style=styles.font_default_style,
+                        font_size=Size.DOUBLE.value,
+                        align="center",
+
+                    ),
+                    align="center",
+                    justify="center",
+                    # bg="orange",
+                    width="100%",
+                    height="100%",
+                ),
+                rx.flex(
+                    rx.text(
+                        description,
+                        style=styles.font_default_style,
+
+                    ),
+                    padding="0 2em",
+                    margin="0 auto",
+                    text_align="justify",
+                    align="center",
+                    # bg="pink",
+                    width="100%",
+                    height="100%",
+                ),
+                rx.vstack(
+                    rx.flex(
+                        rx.text(
+                            gaming_style_1,
+                            style=styles.font_default_style,
+                            color=TextColor.ACCENT
+
+                        ),
+                        align="center",
+                        justify="center",
+                        # bg="yellow",
+                        width="100%",
+                        height="100%",
+                    ),
+                    rx.flex(
+                        rx.text(
+                            gaming_style_2,
+                            style=styles.font_default_style,
+                            color=TextColor.ACCENT
+
+                        ),
+                        align="center",
+                        justify="center",
+                        #                             bg="blue",
+                        width="100%",
+                        height="100%",
+                    ),
+                    rx.flex(
+                        rx.text(
+                            gaming_style_3,
+                            style=styles.font_default_style,
+                            color=TextColor.ACCENT
+
+                        ),
+                        align="center",
+                        justify="center",
+                        #                             bg="maroon",
+                        width="100%",
+                        height="100%",
+                    ),
+                    spacing="1",
+                    height="100%",
+                    width="100%",
+                    #                         bg="olive"
+                ),
+                height="100%",
+                width="60%",
+                # bg="green"
+            ),
 
         ),
-        justify_content="flex-end",
-        height="50%",
-        width="35%",
-        # bg="red"
-    ),
-    rx.vstack(
+    
         rx.flex(
             rx.text(
-                subtitle,
-                style=styles.font_default_style,
-                font_size=Size.DOUBLE.value,
-                align="center",
-
+                "Dark plays. Sharp minds.",
+                margin_right="2em",
+            ),
+            button_rounded(
+                "swords",
+                "svg"
             ),
             align="center",
-            justify="center",
-            # bg="orange",
-            width="100%",
-            height="100%",
+            justify="end",
+            height="4em",
+            width="96%",
+            # bg="violet",
         ),
-        rx.flex(
-            rx.text(
-                description,
-                style=styles.font_default_style,
-
-            ),
-            padding="0 2em",
-            margin="0 auto",
-            text_align="justify",
-            align="center",
-            # bg="pink",
-            width="100%",
-            height="100%",
-        ),
-        rx.vstack(
-            rx.flex(
-                rx.text(
-                    gaming_style_1,
-                    style=styles.font_default_style,
-                    color=TextColor.ACCENT
-
-                ),
-                align="center",
-                justify="center",
-                # bg="yellow",
-                width="100%",
-                height="100%",
-            ),
-            rx.flex(
-                rx.text(
-                    gaming_style_2,
-                    style=styles.font_default_style,
-                    color=TextColor.ACCENT
-
-                ),
-                align="center",
-                justify="center",
-                #                             bg="blue",
-                width="100%",
-                height="100%",
-            ),
-            rx.flex(
-                rx.text(
-                    gaming_style_3,
-                    style=styles.font_default_style,
-                    color=TextColor.ACCENT
-
-                ),
-                align="center",
-                justify="center",
-                #                             bg="maroon",
-                width="100%",
-                height="100%",
-            ),
-            spacing="1",
-            height="100%",
-            width="100%",
-            #                         bg="olive"
-        ),
-        height="50%",
-        width="50%",
-        #                     bg="green"
-    ),
-    rx.vstack(
-        button_rounded(
-            "swords",
-            "svg"
-        ),
-        rx.text(
-            "Únete",
-            style=styles.font_vertical_button_style,
-        ),
-        align="center"
-    ),
-    spacing="3",
-    # height="100%",
-    width="50%",
-    # bg="violet",
-)
+        spacing="3",
+        # height="100%",
+        width="45%",
+        # bg="violet",
+    )
